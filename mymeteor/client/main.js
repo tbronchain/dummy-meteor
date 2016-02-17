08deafd71026c3383c60b6de4c6f92fb9116dd67
Thibault Bronchain <thibault.bronchain@gmail.com>
2016-02-17 15:12:26 +0700

    add some cool stuff (add and save and see db messages

--- mymeteor/client/main.js
+++ mymeteor/client/main.js
@@ -3,3 +3,15 @@ Template.messageList.helpers({
         return Messages.find();
     }
 });
+
+Template.messageForm.events({
+    'click button[data-action=send]': function(e){
+        var name = $('input[data-field=name]').val();
+        var message = $('input[data-field=message]').val();
+        Messages.insert({
+            name: name,
+            message: message
+        });
+        $('input[data-field=message]').val('').focus();
+    }
+});
