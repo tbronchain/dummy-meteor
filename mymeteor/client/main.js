79f60546f6cf9154632341bd2b6352973ddff4c9
Thibault Bronchain <thibault.bronchain@gmail.com>
2016-02-17 16:21:31 +0700

    add session example (last message)

--- mymeteor/client/main.js
+++ mymeteor/client/main.js
@@ -10,12 +10,24 @@ Template.messageList.helpers({
     }
 });
 
+Template.messageForm.helpers({
+    lastMessage: function() {
+        return moment(
+            Session.get('lastMessage')
+        ).format('LTS');
+    }
+});
+
 Template.messageForm.events({
     'keydown input[data-field=message]': function(e){
         if (e.keyCode != 13) {return;}
 
+        // get values from form
         //var name = $('input[data-field=name]').val();
         var message = $('input[data-field=message]').val();
+
+        // Insert into DB
+        Session.set('lastMessage', new Date());
         Messages.insert({
             _owner: Meteor.userId(),
             name: Meteor.user().emails[0].address,
